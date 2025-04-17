657320436e08d4f62ec50f8b0be2507bf3a52410
Cy Strain-Seymour <cystrainseymour@gmail.com>
2025-04-16 20:07:19 -0400

    added index, styles, js

---
+++ kakuro.js
@@ -0,0 +1,56 @@
+function makeBoard(base, height, width, difficulty){
+	const rows=[];
+	for(let i=0; i<height; i+=1){
+		rows[i]=[];
+		preventBigBlocksRow(base, rows, i);
+	}
+	for(let i=0; i<width; i+=1){
+		preventBigBlocksCol(base, rows, i);
+	}
+}
+	
+	
+function preventBigBlocksRow(base, rows, r){
+	let width=rows[0].length;
+	let cur=0;
+	while(width-cur >= base){
+		cur += Math.random()*base;
+		rows[r][cur]=[-1,-1];
+	}
+}
+
+function preventBigBlocksCol(base, rows, c){
+	let height=rows.length;
+	let cur=0;
+	while(height-cur >= base){
+		cur += Math.random()*base;
+		rows[cur][c]=[-1,-1];
+	}
+}
+
+function makeRandom(row, left, right, base){
+	let max= 
+}
+
+function shuffle(array) {
+  let cur = array.length;
+
+  // While there remain elements to shuffle...
+  while (cur != 0) {
+    let r = Math.floor(Math.random() * cur);
+    cur--;
+
+    [array[cur], array[r]] = [
+      array[r], array[cur]];
+  }
+}
+	
+function getNFromAToB(nums, n, a, b){
+	let c=0;
+	for(let i=a; i<=b && c<n; i++){
+		if(Math.floor(Math.random())){
+			nums.append(i);
+			c++;
+		}
+	}
+}
