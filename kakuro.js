a8f203c8721fd4e59442279f6c23227cbac36c67
Cy Strain-Seymour <cystrainseymour@gmail.com>
2025-05-11 11:55:58 -0500

    made getNums()

--- kakuro.js
+++ kakuro.js
@@ -3,6 +3,7 @@ function constructBoard(event){
 	let width=event.target.width.value;
 	document.getElementById("board").remove();
 	let table = document.createElement("table", {id:"board"});
+	let nums = getNums(height, width, 10);
 	
 	for(var i=0; i<height; i++){
 		let row = document.createElement("tr", {id:i});
@@ -11,8 +12,9 @@ function constructBoard(event){
 			
 			cell.appendChild(
 			document.createElement("textarea", 
-			{maxlength:"1", onkeyup:"checkInput(this)"})
+			{id:"text", maxlength:"1", onkeyup:"checkInput(this)"})
 			);
+			cell.getElementById("text").value = nums[i][j];
 			
 			row.appendChild(cell);
 		}
@@ -20,4 +22,38 @@ function constructBoard(event){
 	}
 	var board_cont = document.getElementById("board_cont");
 	board.appendChild(table);
+}
+
+function getNums(height, width, base){
+	
+	const arr = new Array(height).fill(new Array(width));
+	var used = new Array(base).fill(0);
+	
+	for(var i = 0; i < height; i++){
+		for(var j = 0; j < width; j++){
+			var num = Math.Random()*base + 1;
+			if(used[num]){
+				used = new Array(base).fill(0);
+			} else{
+				used[num] = 1;
+			}
+			arr[i][j] = num;
+		}
+	}
+	
+	for(var j = 0; j < width; j++){
+		used = new Array(base).fill(0);
+		for(var  = 0; i < height; i++){
+			if(arr[i][j] === 0){
+				used = new Array(base).fill(0);
+			} else if(used[arr[i][j]]){
+				arr[i][j] = 0;
+				used = new Array(base).fill(0);
+			} else{
+				used[arr[i][j]] = 1;
+			}
+		}
+	}
+	
+	return arr;
 }
